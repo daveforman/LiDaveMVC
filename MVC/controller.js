19172ff15c9468e669792473295ff9e0bc392558
Li Zhang <bylizhang@gmail.com>
2018-03-21 11:47:49 -0700

    commit before modifying view.js

--- MVC/controller.js
+++ MVC/controller.js
@@ -2,7 +2,8 @@ $(document).ready(initApp);
 
 function initApp() {
 	var controller = new Controller();
-	$	
+	var view = new View();
+	$('#submit').on('click', view.getUserAnswers);	
 }
 
 function addClickHandler(argument) {
@@ -11,26 +12,16 @@ function addClickHandler(argument) {
 
 class Controller {
 	constructor() {
-		this.answer = [
-			'A', 'C', 'B'
-		]
+		this.answer = ['A', 'C', 'B']
 	}
 	
-	compareAnswer(inputArr){
+	compareAnswers(inputArr){
 		var correctCounter = 0;
 		for (var i = 0; i < inputArr.length; i++) {
 			if(inputArr[i] === this.answer[i])
 				correctCounter++;
 		}
-		return correctCounter/this.answer.length
-	}
-
-	decideViewOutput(userAnswer){
-		if (compareAnswer(userAnswer) >= 0.7) {
-			view.displayWin();
-		} else {
-			view.displayLoss();
-		}
+		view.displayScore(correctCounter/this.answer.length);
 	}
 }
 
--- MVC/view.js
+++ MVC/view.js
@@ -1,21 +1,21 @@
-class View {
-    constructor(){
+///Example from stack 
 
-    }
-        
-    getCheckedValue( radioName ){
-        var radios = document.getElementsByName( radioName ); // Get radio group by-name
-        for(var y=0; y<radios.length; y++)
-          if(radios[y].checked) return radios[y].value; // return the checked value
-    }
+var answers = ["A","C","B"], 
+    tot = answers.length;
 
-     displayScore(result) {
-        if(result >= 70){
-            console.log("You can be our friend!")
-        } else {
-            console.log("You need to keep studying to be our friend!")
-        }
-      }
-    
+function getCheckedValue( radioName ){
+    var radios = document.getElementsByName( radioName ); // Get radio group by-name
+    for(var y=0; y<radios.length; y++)
+      if(radios[y].checked) return radios[y].value; // return the checked value
 }
 
+function getScore(){
+  var score = 0;
+  for (var i=0; i<tot; i++)
+    if(getCheckedValue("question"+i)===answers[i]) score += 1; // increment only
+  return score;
+}
+
+function returnScore(){
+  alert("Your score is "+ getScore() +"/"+ tot);
+}
